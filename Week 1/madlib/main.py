eb718b933d95236cd94c21f082d8f6d83d02bd32
Justin Henry <doc2@fullsail.edu>
2014-08-08 21:49:27 -0500

    Created function

--- Week 1/madlib/main.py
+++ Week 1/madlib/main.py
@@ -3,7 +3,12 @@ adjective = raw_input("Enter an adjective ")
 verb = raw_input("Enter a verb ")
 num1 = raw_input("Enter a number ")
 num2 = raw_input("Enter another number ")
-num3 = raw_input("Enter one more number ")
+num3 = raw_input("Enter a smaller number than before ")
 words = dict() #create dictionary object
 words = {"Noun": noun, "Adjective": adjective, "Verb": verb}
 numbers = [num1, num2, num3]
+
+def calcQuo(a, b): #create function
+    quo = int(a)/int(b) #turn parameters into integers so they can be divided
+    return quo #return the quotient
+per = calcQuo(num2, num3); #run function with variable parameters
