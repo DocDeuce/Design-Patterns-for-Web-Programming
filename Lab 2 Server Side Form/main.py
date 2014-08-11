ad1852af404623e70ffd7ed792de6dd917b191dc
Justin Henry <doc2@fullsail.edu>
2014-08-10 22:58:07 -0500

    Beginning html

--- Lab 2 Server Side Form/main.py
+++ Lab 2 Server Side Form/main.py
@@ -8,7 +8,19 @@ import webapp2
 
 class MainHandler(webapp2.RequestHandler): #Declaring a class
     def get(self): #The function to start everything
-        
+        page_head = '''<!DOCTYPE html> 
+<html>
+    <head>
+        <title>Simple Form</title>
+    </head>
+    <body>'''
+
+        page_body = '''
+        '''
+
+        page_close = '''
+    </body>
+</html>'''
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
