53f967156e97b1a31412a902b022a7f6a093c320
Justin Henry <doc2@fullsail.edu>
2014-08-10 23:44:37 -0500

    Created form

--- Lab 2 Server Side Form/main.py
+++ Lab 2 Server Side Form/main.py
@@ -8,7 +8,7 @@ import webapp2
 
 class MainHandler(webapp2.RequestHandler): #Declaring a class
     def get(self): #The function to start everything
-        page_head = '''<!DOCTYPE html> 
+        page_head = '''<!DOCTYPE html>
 <html>
     <head>
         <title>Simple Form</title>
@@ -16,11 +16,24 @@ class MainHandler(webapp2.RequestHandler): #Declaring a class
     <body>'''
 
         page_body = '''
-        '''
+        <form method="GET" action="">
+            <label>Name: </label><input type="text" name="user">
+            <label>Gender: </label><select>
+                <option>Male</option>
+                <option>Female</option>
+            </select><br/>
+            <label>Email: </label><input type="text" name="email"><br/>
+            <label>Destination: </label><input type="text" name="destination"><br/>
+            <p>What is the reason for your travel? </p><br/>
+            <label>Business</label><input type="checkbox" name="purpose" value="business"><br/>
+            <label>Pleasure</label><input type="checkbox" name="purpose" value="pleasure">
+        </form>'''
 
         page_close = '''
     </body>
 </html>'''
+
+        self.response.write(page_head + page_body + page_close)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
