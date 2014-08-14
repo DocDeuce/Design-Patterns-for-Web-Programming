2f4c288d300d273038b8bfd8982d01fd8ed516b8
Justin Henry <doc2@fullsail.edu>
2014-08-14 00:55:38 -0500

    Tested calculation for total with numbers

--- encapsulated-calculator/main.py
+++ encapsulated-calculator/main.py
@@ -11,43 +11,44 @@ class MainHandler(webapp2.RequestHandler):
         pass
         #Mal
         m = Earnings()
-        m.mon =
-        m.tue =
-        m.wed =
-        m.thu =
-        m.fri =
+        m.mon = 150
+        m.tue = 320
+        m.wed = 85
+        m.thu = 202
+        m.fri = 237
+        self.response.write(m.total)
 
         #Zoe
         z = Earnings()
-        z.mon =
-        z.tue =
-        z.wed =
-        z.thu =
-        z.fri =
+        z.mon = 176
+        z.tue = 278
+        z.wed = 76
+        z.thu = 189
+        z.fri = 255
 
         #Wash
         w = Earnings()
-        w.mon =
-        w.tue =
-        w.wed =
-        w.thu =
-        w.fri =
+        w.mon = 97
+        w.tue = 156
+        w.wed = 54
+        w.thu = 101
+        w.fri = 166
 
         #Kaylee
         k = Earnings()
-        k.mon =
-        k.tue =
-        k.wed =
-        k.thu =
-        k.fri =
+        k.mon = 92
+        k.tue = 123
+        k.wed = 48
+        k.thu = 88
+        k.fri = 115
 
         #Jayne
         j = Earnings()
-        j.mon =
-        j.tue =
-        j.wed =
-        j.thu =
-        j.fri =
+        j.mon = 142
+        j.tue = 258
+        j.wed = 72
+        j.thu = 163
+        j.fri = 240
 
 class Earnings(object):
     def __init__(self):
@@ -56,13 +57,13 @@ class Earnings(object):
         self.wed = 0
         self.thu = 0
         self.fri = 0
-        self.total = 0
+        self.__total = 0
         self.__contribution = 0
 
     @property
     def total(self):
-        self.total = self.mon + self.tue + self.wed + self.thu + self.fri
-        return self.total
+        self.__total = self.mon + self.tue + self.wed + self.thu + self.fri
+        return self.__total
 
     @property
     def contribution(self):
