c728b65d9ccc7fa3f7a6efd904740c5af6ae2c7f
Justin Henry <doc2@fullsail.edu>
2014-08-20 10:22:13 -0500

    It works!

--- encapsulated-calculator/main.py
+++ encapsulated-calculator/main.py
@@ -10,33 +10,50 @@ from pages import Page
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         g = Page()
-
-        self.response.write(g.page_head)
-
-
         crew = {"Mal":[150,320,85,202,237],
                 "Zoe":[176,278,76,189,255],
                 "Wash":[97,156,54,101,166],
                 "Kaylee":[92,123,48,88,115],
                 "Jayne":[142,258,72,163,240]}
 
-        for c in crew:
-            g.who = c
-            g.comp = crew.get(c, "none")
-            g.total_spot = ""
-            self.response.write(g.crew_build() + g.calculate())
-
-
+        #self.response.write(g.page_head)
 
         if self.request.GET:
-            print "working"
-            r = self.request.GET
-            e = Earnings()
-            e.pay = [int(r["mon"]),int(r["tue"]),int(r["wed"]),int(r["thu"]),int(r["fri"])]
-            e.calc_total()
-            g.total_spot = #e.total
+            self.response.write(g.page_head)
 
-        self.response.write(g.page_close)
+            r = self.request.GET
+
+
+
+            g.whom = r["who"]
+            #g.indi_build()
+            e = Earnings()
+
+            e.pay = [int(r["mon"]),int(r["tue"]),int(r["wed"]),int(r["thu"]),int(r["fri"])]
+            g.pay = [int(r["mon"]),int(r["tue"]),int(r["wed"]),int(r["thu"]),int(r["fri"])]
+            e.calc_total()
+            g.total_spot = "Total: " + str(e.total)
+            #print g.who
+            self.response.write(g.indi_build() + g.p_begin + str(g.total_spot) + g.p_end + g.div_end + g.page_close)
+
+            #self.response.write(g.whole_page)
+            #self.response.write(e.total)
+            #g.who = "Me"
+
+        else:
+            self.response.write(g.page_head)
+
+            for c in crew:
+                g.who = c
+                g.comp = crew.get(c, "none")
+                self.response.write(g.crew_build() + g.calculate() + g.div_end)
+
+            self.response.write(g.page_close)
+
+
+
+
+        #self.response.write(g.page_close)
 
 class Earnings(object):
     def __init__(self):
--- encapsulated-calculator/pages.py
+++ encapsulated-calculator/pages.py
@@ -6,8 +6,8 @@ class Page(object):
         <title>Encapsulated Calculator</title>
         <link href="css/style.css" rel="stylesheet" type="text/css" />
     </head>
-    <body>'''
-
+    <body>
+        '''
         self.crew_show = '''
         <div>
             <h2>{self.who}</h2>
@@ -17,9 +17,19 @@ class Page(object):
             <p>Thu: {self.comp[3]}</p><br/>
             <p>Fri: {self.comp[4]}</p><br/>
         '''
+        self.indi_show = '''
+        <div>
+            <h2>{self.whom}</h2>
+            <p>Mon: {self.pay[0]}</p><br/>
+            <p>Tue: {self.pay[1]}</p><br/>
+            <p>Wed: {self.pay[2]}</p><br/>
+            <p>Thu: {self.pay[3]}</p><br/>
+            <p>Fri: {self.pay[4]}</p><br/>
+        '''
 
         self.page_form = '''
             <form method="GET" action="">
+                <input type="hidden" name="who" value="{self.who}">
                 <input type="hidden" name="mon" value="{self.comp[0]}">
                 <input type="hidden" name="tue" value="{self.comp[1]}">
                 <input type="hidden" name="wed" value="{self.comp[2]}">
@@ -27,30 +37,46 @@ class Page(object):
                 <input type="hidden" name="fri" value="{self.comp[4]}">
                 <input type="submit" value="Total">
             </form>
-            <div>{self.total_spot}</div>
-        </div>'''
-
+        '''
+        self.p_begin = '''
+            <p>'''
+        self.__total_spot = ""
+        self.p_end = '''
+            </p>'''
+        self.div_end = '''
+        </div>
+        '''
         self.page_close = '''
     </body>
 </html>'''
-
-    def display(self):
-        all = self.page_head + self.page_close
-        all = all.format(**locals())
-        return all
+        self.whole_page = ""
 
     def calculate(self):
         crew_comp = self.page_form
         crew_comp = crew_comp.format(**locals())
         return crew_comp
 
-
     def crew_build(self):
-        '''crew_comp = self.page_form
-        crew_comp = crew_comp.format(**locals())'''
         crew_div = self.crew_show
         crew_div = crew_div.format(**locals())
         return crew_div
 
+    def indi_build(self):
+        indi_div = self.indi_show
+        indi_div = indi_div.format(**locals())
+        return indi_div
+
+    def update(self):
+        self.whole_page = self.page_head + self.indi_show + self.p_begin + self.total_spot + self.p_end + self.div_end + self.page_close
+        self.whole_page = self.whole_page.format(**locals())
+
+    @property
+    def total_spot(self):
+        return self.__total_spot
+
+    @total_spot.setter
+    def total_spot(self, added):
+        self.__total_spot = added
+        #self.indi_build()
 
 
