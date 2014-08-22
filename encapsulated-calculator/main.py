d74be8b0f6d9fe6667e22045828897c3f0d47036
Justin Henry <doc2@fullsail.edu>
2014-08-21 21:11:32 -0500

    Added comments and deleted unused code

--- encapsulated-calculator/main.py
+++ encapsulated-calculator/main.py
@@ -9,45 +9,45 @@ from pages import Page
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        g = Page()
+        g = Page() #Set the Page object as easily accessible
         crew = {"Mal":[150,320,85,202,237],
                 "Zoe":[176,278,76,189,255],
                 "Wash":[97,156,54,101,166],
                 "Kaylee":[92,123,48,88,115],
-                "Jayne":[142,258,72,163,240]}
-        if self.request.GET:
-            self.response.write(g.page_head)
-            r = self.request.GET
-            g.whom = r["who"]
-            e = Earnings()
-            e.pay = [int(r["mon"]),int(r["tue"]),int(r["wed"]),int(r["thu"]),int(r["fri"])]
-            g.pay = [int(r["mon"]),int(r["tue"]),int(r["wed"]),int(r["thu"]),int(r["fri"])]
-            e.calc_total()
-            g.total_spot = "Total: " + str(e.total)
-            self.response.write(g.indi_build() + g.p_begin + str(g.total_spot) + g.p_end + g.div_end + g.page_close)
-        else:
-            self.response.write(g.page_head)
-            for c in crew:
-                g.who = c
-                g.comp = crew.get(c, "none")
-                self.response.write(g.crew_build() + g.calculate() + g.div_end)
-            self.response.write(g.page_close)
+                "Jayne":[142,258,72,163,240]} #This dictionary contains five crew members of the ship Serenity and their pay for each day of a work week
+        if self.request.GET: #If form data has been submitted, run the following
+            self.response.write(g.page_head) #This writes the opening html, and body tag, and also contains the page title and the css link
+            r = self.request.GET #Set the request as easily accessible
+            g.whom = r["who"] #This sets the whom value of the Page object to the value named by the form as "who"
+            e = Earnings() #Set the earnings method as easily accessible
+            e.pay = [int(r["mon"]),int(r["tue"]),int(r["wed"]),int(r["thu"]),int(r["fri"])] #This creates an array to be sent into the Earnings method
+            g.pay = [int(r["mon"]),int(r["tue"]),int(r["wed"]),int(r["thu"]),int(r["fri"])] #This creates the same array to be sent into the Page Object
+            e.calc_total() #This requests that the method for calculating the total weekly compensation be called from the Earnings object
+            g.total_spot = "Total: " + str(e.total) #Uses the setter in the Page object to display the total calculated by the Earnings object
+            self.response.write(g.indi_build() + g.p_begin + str(g.total_spot) + g.p_end + g.div_end + g.page_close) #Writes the individual crew member, ther pay per day, and their total pay for the week
+        else: #Without form data being submitted, do the following
+            self.response.write(g.page_head) #This writes the opening html, and body tag, and also contains the page title and the css link
+            for c in crew: #Begin iterating through the crew dictionary
+                g.who = c #Sets the value of "who" in the Page object the key in the index of c in the dictionary
+                g.comp = crew.get(c, "none") #This returns an array of all of the number values associated with the crew member at the current index and sets the it as the value of "comp" in the Page object
+                self.response.write(g.crew_build() + g.calculate() + g.div_end) #run the method to display crew member information in a div and then close that div
+            self.response.write(g.page_close) #Close the html and body tag
 
-class Earnings(object):
-    def __init__(self):
-        self.pay = 0
-        self.__total = 0
+class Earnings(object): #This class handles the numbers from the dictionary
+    def __init__(self): #Initiate yourself!
+        self.pay = 0 #Establishes the variable to have its value set by the MainHandler
+        self.__total = 0 #Private total to be calculated upon method call
 
-    @property
+    @property #A decorator to establish a getter
     def total(self):
-        return self.__total
+        return self.__total #This returns a private total from a public method
 
-    @total.setter
-    def total(self, adjustment):
+    @total.setter #An unused setter, just in case Captain Mal wants to make some pay adjustments
+    def total(self, adjustment): #The setter requires two parameters, I called the second one adjustment
         self.__total = adjustment
 
-    def calc_total(self):
-        self.__total = sum(self.pay)
+    def calc_total(self): #Method to be called in order to calculate the total weekly compensation
+        self.__total = sum(self.pay) #Deduces the sum of all values in an array and sets it as the private total
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
--- encapsulated-calculator/pages.py
+++ encapsulated-calculator/pages.py
@@ -1,5 +1,15 @@
-class Page(object):
-    def __init__(self):
+'''
+Justin Henry
+8/13/2014
+DPW - Online
+Encapsulated Calculator
+'''
+
+class Page(object): #Page object to build html and to be imported into the main handler
+
+    def __init__(self): #Initiate yourself!
+
+        #The head of the html establishing the document type and title, linking the css, and opening the body tag
         self.page_head = '''<!DOCTYPE html>
 <html>
     <head>
@@ -8,6 +18,8 @@ class Page(object):
     </head>
     <body>
         '''
+
+        #A div to contain and display each crew member's name and earnings for each weekday
         self.crew_show = '''
         <div>
             <h2>{self.who}</h2>
@@ -16,7 +28,9 @@ class Page(object):
             <p>Wed: {self.comp[2]}</p><br/>
             <p>Thu: {self.comp[3]}</p><br/>
             <p>Fri: {self.comp[4]}</p><br/>
-        '''
+        ''' #A loop was used to iterate though a dictionary of crew members and their compensation, hence the brackets
+
+        #This div displays the individual who's total weekly compensation was requested, it has the same format as crew show but pulls its information from a form
         self.indi_show = '''
         <div>
             <h2>{self.whom}</h2>
@@ -26,7 +40,7 @@ class Page(object):
             <p>Thu: {self.pay[3]}</p><br/>
             <p>Fri: {self.pay[4]}</p><br/>
         '''
-
+        #This form contains the crew names and pay amounts from the dictionary in order to submit it but hides the inputs to receive hard coded data
         self.page_form = '''
             <form method="GET" action="">
                 <input type="hidden" name="who" value="{self.who}">
@@ -38,45 +52,56 @@ class Page(object):
                 <input type="submit" value="Total">
             </form>
         '''
+        #an opening paragraph tag to contain the total that will eventually be displayed
         self.p_begin = '''
             <p>'''
+
+        #An empty string whose value will be set upon form submission, displaying total weekly compensation
         self.__total_spot = ""
+
+        #Closing paragraph tag to end the total display
         self.p_end = '''
             </p>'''
+
+        #Closing the div that contains the crew member and their numbers
         self.div_end = '''
         </div>
         '''
+
+        #This closes the html body and the html, ending the page
         self.page_close = '''
     </body>
 </html>'''
+
+        #I don't even need this
         self.whole_page = ""
 
+    #This method allows the form on this page to have its values set as the dictionary information from the main file in order to calculate the total compensation
     def calculate(self):
         crew_comp = self.page_form
         crew_comp = crew_comp.format(**locals())
         return crew_comp
 
+    #This method allows the div on this page to have its values set as the dictionary information from the main file in order to display it
     def crew_build(self):
         crew_div = self.crew_show
         crew_div = crew_div.format(**locals())
         return crew_div
 
+    #This method allows the resulting div on this page to have its values set as the information from the main submitted form in order to display it
     def indi_build(self):
         indi_div = self.indi_show
         indi_div = indi_div.format(**locals())
         return indi_div
 
-    def update(self):
-        self.whole_page = self.page_head + self.indi_show + self.p_begin + self.total_spot + self.p_end + self.div_end + self.page_close
-        self.whole_page = self.whole_page.format(**locals())
-
+    #This is a getter of the total that the for calculates, it returns the private information but is called upon as a method
     @property
     def total_spot(self):
         return self.__total_spot
 
+    #This is a setter of the private total that the form calculates
     @total_spot.setter
     def total_spot(self, added):
         self.__total_spot = added
-        #self.indi_build()
 
 
