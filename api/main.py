b750071822ae0d5efbfdd16028277d61a86bdfda
Justin Henry <doc2@fullsail.edu>
2014-08-22 21:43:09 -0500

    Added comments

--- api/main.py
+++ api/main.py
@@ -5,24 +5,24 @@ DPW - Online
 Proof of Concept
 '''
 import webapp2
-import urllib2
-from xml.dom import minidom
+import urllib2 #Necessary to request, receive, and open data
+from xml.dom import minidom # Used to parse through received data
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        p = FormPage()
-        p.input = [['text', 'zip', 'Zip Code'], ['submit', 'Submit']]
-        self.response.write(p.display())
-        if self.request.GET:
-            zip = self.request.GET['zip']
-            url = "http://whoismyrepresentative.com/getall_mems.php?zip=" + zip
-            request = urllib2.Request(url)
-            opener = urllib2.build_opener()
-            result = opener.open(request)
-            xmldoc = minidom.parse(result)
-            self.content = "<br/>"
-            list = xmldoc.getElementsByTagName('rep')
-            for item in list:
+        p = FormPage() #Calls the subclass of the superclass "Form"
+        p.input = [['text', 'zip', 'Zip Code'], ['submit', 'Submit']] #An array of arrays used to set html input attributes
+        self.response.write(p.display()) #displays html page from FormPage class
+        if self.request.GET: #If form data has ben submitted, do the following
+            zip = self.request.GET['zip'] #sets zip variable value to what is received from input
+            url = "http://whoismyrepresentative.com/getall_mems.php?zip=" + zip #For accessing the api information and the variable "zip" to be submitted via html form input
+            request = urllib2.Request(url) #Sets variable for urllib2 to request data from the url listed above
+            opener = urllib2.build_opener() #Creates object to receive data from url
+            result = opener.open(request) #Use the receiving object to open the data requested from the url
+            xmldoc = minidom.parse(result) #Parse the result of the request
+            self.content = "<br/>" #Initiate the display of the received data
+            list = xmldoc.getElementsByTagName('rep') #Creates an array of the primary objects received from the request
+            for item in list: #Iterate though the list of results and do the following for each to build and display the information
                 self.content += "Name: "+item.attributes["name"].value
                 self.content += " Party: "+item.attributes["party"].value
                 self.content += " District: "+item.attributes["district"].value
@@ -31,9 +31,9 @@ class MainHandler(webapp2.RequestHandler):
                 self.content += " Website: "+item.attributes["link"].value
                 self.content += "<br/>"
 
-            self.response.write(self.content)
+            self.response.write(self.content) #Display the accumulated content
 
-class Page(object):
+class Page(object): #A superclass to contain the necessary html for building a page
     def __init__(self):
         self._page_head = '''
 <!DOCTYPE html>
@@ -48,34 +48,31 @@ class Page(object):
     </body>
 </html>'''
 
-    def display(self):
-        return self._page_head + self._page_body + self._page_close
-
-class FormPage(Page):
+class FormPage(Page): #A subclass of the Page superclass that creates the html form
     def __init__(self):
-        super(FormPage, self).__init__()
+        super(FormPage, self).__init__() #The superclass for this subclass is Page, get it going
         self._form_open = '''
         <form method="GET">'''
-        self.__input = []
+        self.__input = [] #An empty array to contain
         self._form_inputs = ""
         self._form_close = '''
         </form>'''
 
-    @property
+    @property #Unused but necessary getter
     def input(self):
         pass
 
     @input.setter
     def input(self, arr):
-        self.__input = arr
-        for item in arr:
-            self._form_inputs += '<input type="' + item[0] + '" name="' + item[1]
-            try:
-                self._form_inputs += '" placeholder="' + item[2] +'" />'
-            except:
-                self._form_inputs += '" />'
+        self.__input = arr #Access the private variable
+        for item in arr: #Iterate through the array and perform the follwing
+            self._form_inputs += '<input type="' + item[0] + '" name="' + item[1] #creates html input and sets attribute values
+            try: #Attemt to do the following if the means are available
+                self._form_inputs += '" placeholder="' + item[2] +'" />' #Adds placeholder attribute and its value to input and closes it
+            except: #if the above cannot be done, do the following
+                self._form_inputs += '" />' #Close the input tag
 
-    def display(self):
+    def display(self): #Method for displaying html page
         return self._page_head + self._page_body + self._form_open + self._form_inputs + self._form_close + self._page_close
 
 app = webapp2.WSGIApplication([
